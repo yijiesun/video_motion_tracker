e22ce5d8f3d548899a3a04438fc2f593d4bd8d5b
SYJ <yijie.sun@rock-chips.com>
2017-05-18 14:59:17 +0800

    appreaciate release version

---
+++ get_video_frame.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef GET_VIDEO_FRAME_H
+#define GET_VIDEO_FRAME_H
+
+#include "include.h"
+
+int get_video_frame(char* video_address, char* frame_address);
+
+#endif  // !GET_VIDEO_FRAME_H
